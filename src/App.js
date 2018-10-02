05d548c0f91c517e479aa1f35b3e6b19daeb0820
ender503 <ender503.tw@gmail.com>
2018-10-02 18:57:31 +0800

    Add Header component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import { Button } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
