210465c74de5d0f3ee6cf4d9e2f1c8d733676407
Yillkid <yillkid@biilabs.io>
2018-10-09 03:03:31 +0000

    Fix image url

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,15 +7,48 @@ class Home extends React.Component {
       <Container className="main-container">
         <div>
           <Jumbotron>
-            <h1 className="display-3">Hello, world!</h1>
-            <p className="lead">
-              This is a simple hero unit, a simple Jumbotron-style component for
-              calling extra attention to featured content or information.
+            <p className="lead">Three Elements in TangleID</p>
+            <p className="my-2">
+              Basically, there are three main players in TangleID: Issuer,
+              Holder and Inspector-Verifier. Sometimes, to further deliberate
+              the framework, Identifier Registry will be emphasized, but here we
+              focus on major three elements and give them descriptions in
+              reference of W3C.
             </p>
-            <hr className="my-2" />
-            <p>
-              It uses utility classes for typography and spacing to space
-              content out within the larger container.
+            <p className="my-2">
+              <img
+                src="/static/imgs/vc_roles.4fb3f701.png"
+                width="60%"
+                height="60%"
+              />
+            </p>
+            <p className="my-2">
+              <b>1. Issuer</b>
+            </p>
+            <p className="my-2">
+              The entity that creates a claim and associates it with a
+              particular subject (Often known as Holder, but not always).
+            </p>
+            <p className="my-2">
+              <b>2. Holder</b>
+            </p>
+            The entity who controls a particular claim. A holder is typically
+            the initiator of the transmission of a claim.
+            <br />
+            <p className="my-2">
+              <b>3. Inspector-Verifier</b>
+            </p>
+            <p className="my-2">
+              The entity verifying a claim about a given subject.
+            </p>
+            <p className="my-2">
+              In the certification scenario, Issuer is the authorites; Holder is
+              you; Inspector Verifier is the bank. The flow can be described as
+              follow: Holder (you) applies for the loan, hence the identity
+              authentication to Issuer (the authorities) by Inspector Verifier
+              (bank). That is, TangleID, a powerful tool, can reduce the costs
+              by M2M mechanics and further rebuild the trust without trusts by
+              DLT.
             </p>
             <p className="lead">
               <Button color="primary">Learn More</Button>
